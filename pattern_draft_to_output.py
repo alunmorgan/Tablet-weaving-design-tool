d7ed820c38322ae3ec444045ddf84632bb86275c
alunmorgan <alun@curiousidea.co.uk>
2018-05-12 12:20:41 +0100

    Added input testing for initialisation of teh card class, and relavant unit tests.

--- pattern_draft_to_output.py
+++ pattern_draft_to_output.py
@@ -2,13 +2,26 @@
 
 class Card:
 
-    def __init__(self, number_of_holes, list_of_thread_colours, list_of_thread_types, list_of_threading_directions):
+    def __init__(self, number_of_holes, threading_direction, list_of_thread_colours, list_of_thread_types):
+        if type(number_of_holes) is not int:
+            raise(TypeError, 'number of holes needs to be an int')
+        if len(list_of_thread_colours) != number_of_holes:
+            raise(ValueError, 'Incorrect number of thread colours assigned')
+        if len(list_of_thread_types) != number_of_holes:
+            raise(ValueError, 'Incorrect number of thread types assigned')
+        if type(threading_direction) is not str:
+            raise(TypeError, 'The threading direction needs to be a string')
+        if threading_direction != 'LTR' and threading_direction != 'RTL' and \
+           threading_direction != 'ltr' and threading_direction != 'rtl':
+            raise(ValueError, 'The threading directions needs to be RTL or LTR')
+
         self.N_holes = number_of_holes
         self.thread_cols = list_of_thread_colours
         self.thread_types = list_of_thread_types
-        self.thread_direction = list_of_threading_directions
+        self.thread_direction = threading_direction
         self.visible_top = 0
         self.visible_bottom = 1
+        self.prev_turn = 0  # 0 is neutral, away is 1 and towards is -1
 
     def turn_card(self, direction):
         pass
@@ -18,7 +31,7 @@ class Card:
 
 
 card1 = Card(number_of_holes=4,
+             threading_direction='LTR',
              list_of_thread_colours=['Red', 'Red', 'Yellow', 'White'],
-             list_of_thread_types=['cotton', 'cotton', 'cotton', 'cotton'],
-             list_of_threading_directions=['Forward', 'Forward', 'Forward', 'Forward']
+             list_of_thread_types=['cotton', 'cotton', 'cotton', 'cotton']
              )
--- unit_tests_for_pattern_draft_to_output.py
+++ unit_tests_for_pattern_draft_to_output.py
@@ -1,2 +1,84 @@
+import pattern_draft_to_output
+import unittest
 
 
+class TestCardInstantiation(unittest.TestCase):
+
+    def setUp(self):
+        self.expected_thread_colours = ['RRR', 'BBB', 'YYY', 'WWW']
+        self.expected_thread_types = ['CCC', 'CCC', 'CCC', 'CCC']
+        self.expected_number_of_holes = 4
+
+    def test_passes_with_expected_input(self):
+        thread_dirs = ['LTR', 'RTL', 'ltr', 'rtl']  # All the valid inputs for threading direction.
+        for thread_dir in thread_dirs:
+            test_card = pattern_draft_to_output.Card(number_of_holes=self.expected_number_of_holes,
+                                                     threading_direction=thread_dir,
+                                                     list_of_thread_colours=self.expected_thread_colours,
+                                                     list_of_thread_types=self.expected_thread_types
+                                                     )
+            self.assertEqual(test_card.N_holes, self.expected_number_of_holes)
+            self.assertEqual(test_card.thread_direction, thread_dir)
+            self.assertListEqual(test_card.thread_cols, self.expected_thread_colours)
+            self.assertListEqual(test_card.thread_types, self.expected_thread_types)
+
+    def test_raises_TypeError_with_not_int_for_number_of_holes (self):
+        self.assertRaises(TypeError,
+                          pattern_draft_to_output.Card, number_of_holes='4',
+                                                        threading_direction='LTR',
+                                                        list_of_thread_colours=self.expected_thread_colours,
+                                                        list_of_thread_types=self.expected_thread_types
+                          )
+
+    def test_raises_TypeError_with_not_str_for_threading_direction(self):
+        self.assertRaises(TypeError,
+                          pattern_draft_to_output.Card, number_of_holes=self.expected_number_of_holes,
+                                                        threading_direction=['LTR'],
+                                                        list_of_thread_colours=self.expected_thread_colours,
+                                                        list_of_thread_types=self.expected_thread_types
+                          )
+
+    def test_raises_ValueError_with_incorrect_str_for_threading_direction(self):
+        self.assertRaises(ValueError,
+                          pattern_draft_to_output.Card, number_of_holes=self.expected_number_of_holes,
+                                                        threading_direction='AAA',
+                                                        list_of_thread_colours=self.expected_thread_colours,
+                                                        list_of_thread_types=self.expected_thread_types
+                          )
+
+    def test_raises_ValueError_with_incorrect_length_for_list_of_thread_colours(self):
+        self.assertRaises(ValueError,
+                          pattern_draft_to_output.Card, number_of_holes=self.expected_number_of_holes,
+                                                        threading_direction='LTR',
+                                                        list_of_thread_colours=['RRR', 'BBB', 'YYY'],
+                                                        list_of_thread_types=self.expected_thread_types
+                          )
+        self.assertRaises(ValueError,
+                          pattern_draft_to_output.Card, number_of_holes=self.expected_number_of_holes,
+                                                        threading_direction='LTR',
+                                                        list_of_thread_colours=['RRR', 'BBB', 'YYY', 'WWW', 'XXX'],
+                                                        list_of_thread_types=self.expected_thread_types
+                          )
+
+    def test_raises_ValueError_with_incorrect_length_for_list_of_thread_types(self):
+        self.assertRaises(ValueError,
+                          pattern_draft_to_output.Card, number_of_holes=self.expected_number_of_holes,
+                                                        threading_direction='LTR',
+                                                        list_of_thread_colours=self.expected_thread_colours,
+                                                        list_of_thread_types=['CCC', 'CCC', 'CCC']
+                          )
+        self.assertRaises(ValueError,
+                          pattern_draft_to_output.Card, number_of_holes=self.expected_number_of_holes,
+                                                        threading_direction='LTR',
+                                                        list_of_thread_colours=self.expected_thread_colours,
+                                                        list_of_thread_types=['CCC', 'CCC', 'CCC', 'CCC', 'XXX']
+                          )
+
+
+class TestCardFunctions(unittest.TestCase):
+    def setUp(self):
+        self.test_card = pattern_draft_to_output.Card(number_of_holes=4,
+                                                      threading_direction='LTR',
+                                                      list_of_thread_colours=['Red', 'Blue', 'Yellow', 'White'],
+                                                      list_of_thread_types=['cotton', 'cotton', 'cotton', 'cotton']
+                                                      )
