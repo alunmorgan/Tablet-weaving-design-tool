8036f85f305bef5145cd446ac98be1ea9ebd1539
alunmorgan <alun@curiousidea.co.uk>
2018-05-17 15:32:51 +0100

    Added code for turning card.

--- pattern_draft_to_output.py
+++ pattern_draft_to_output.py
@@ -19,12 +19,41 @@ class Card:
         self.thread_cols = list_of_thread_colours
         self.thread_types = list_of_thread_types
         self.thread_direction = threading_direction
-        self.visible_top = 0
-        self.visible_bottom = 1
+        self.visible_top = 0  # Starting location of top visible thread.
+        self.visible_bottom = 1  # Starting location of bottom visible thread.
         self.prev_turn = 0  # 0 is neutral, away is 1 and towards is -1
 
     def turn_card(self, direction):
-        pass
+        if not isinstance(direction, int):
+            raise(TypeError, 'The direction needs to be an integer (Away=1 Towards=-1)')
+
+        if direction != 1 and direction != -1:
+            raise(ValueError, 'The direction needs to be 1 or -1 (Away, Towards)')
+
+        if direction == 1:
+            if self.prev_turn == 1:  # Carrying on in the same direction.
+                self.visible_top += 1
+                self.visible_bottom += 1
+
+            elif self.prev_turn == 0:  # Starting from neutral
+                # Visible top remains the same
+                self.visible_bottom += 1
+            #  No need for the case where self.prev_turn == -1
+            #  as the visibility of the threads remains the same over a reversal of direction
+        elif direction == -1:
+            if self.prev_turn == -1:  # Carrying on in the same direction.
+                self.visible_top += -1
+                self.visible_bottom += -1
+            elif self.prev_turn == 0:  # Starting from neutral
+                self.visible_top += -1
+                # Visible bottom remains the same
+            #  No need for the case where self.prev_turn == -1
+            #  as the visibility of the threads remains the same over a reversal of direction
+
+        # Wrap the values around so theya re always withing the range 0 - self.N_holes.
+        self.visible_top = self.visible_top % self.N_holes
+        self.visible_bottom = self.visible_bottom % self.N_holes
+        self.prev_turn = direction
 
     def flip_card(self):
         pass
--- unit_tests_for_pattern_draft_to_output.py
+++ unit_tests_for_pattern_draft_to_output.py
@@ -82,3 +82,67 @@ class TestCardFunctions(unittest.TestCase):
                                                       list_of_thread_colours=['Red', 'Blue', 'Yellow', 'White'],
                                                       list_of_thread_types=['cotton', 'cotton', 'cotton', 'cotton']
                                                       )
+
+    def test_raises_typeerror_with_str_input(self):
+        self.assertRaises(TypeError, self.test_card.turn_card, direction='AAA')
+
+    def test_passes_with_int_input(self):
+        self.test_card.turn_card(1)
+
+    def test_raises_typeerror_with_float_input(self):
+        self.assertRaises(TypeError, self.test_card.turn_card, direction=1.111)
+
+    def test_neutral_to_away_gives_expected_behaviour(self):
+        # Top should go from 0 to 0
+        # Bottom should go from 1 to 2
+        self.test_card.turn_card(1)
+        self.assertEqual(self.test_card.visible_top, 0)
+        self.assertEqual(self.test_card.visible_bottom, 2)
+
+    def test_neutral_to_towards_gives_expected_behaviour(self):
+        # Top should go from 0 to 3
+        # Bottom should go from 1 to 1
+        self.test_card.turn_card(-1)
+        self.assertEqual(self.test_card.visible_top, 3)
+        self.assertEqual(self.test_card.visible_bottom, 1)
+
+    def test_away_to_away_gives_expected_behaviour(self):
+        # Top should go from 0 to 0 then to 1
+        # Bottom should go from 1 to 2 then to 3
+        self.test_card.turn_card(1)
+        self.test_card.turn_card(1)
+        self.assertEqual(self.test_card.visible_top, 1)
+        self.assertEqual(self.test_card.visible_bottom, 3)
+
+    def test_towards_to_towards_gives_expected_behaviour(self):
+        # Top should go from 0 to 3 then to 2
+        # Bottom should go from 1 to 1 then to 0
+        self.test_card.turn_card(-1)
+        self.test_card.turn_card(-1)
+        self.assertEqual(self.test_card.visible_top, 2)
+        self.assertEqual(self.test_card.visible_bottom, 0)
+
+    def test_towards_to_away_gives_expected_behaviour(self):
+        # Both should stay the same over the reversal
+        # Top should go from 0 to 3 then to 2 and then to 2
+        # Bottom should go from 1 to 1 then to 0 and then to 0
+        self.test_card.turn_card(-1)  # Neutral --> Towards
+        self.test_card.turn_card(-1)  # Towards --> Towards
+        self.test_card.turn_card(1)   # Towards --> Away
+        self.assertEqual(self.test_card.visible_top, 2)
+        self.assertEqual(self.test_card.visible_bottom, 0)
+
+    def test_away_to_towards_gives_expected_behaviour(self):
+        # Both should stay the same over the reversal
+        # Top should go from 0 to 0 then to 1 and then to 1
+        # Bottom should go from 1 to 2 then to 3 and then to 3
+        self.test_card.turn_card(1)   # Neutral --> Away
+        self.test_card.turn_card(1)   # Away --> Away
+        self.test_card.turn_card(-1)  # Away --> Towards
+        self.assertEqual(self.test_card.visible_top, 1)
+        self.assertEqual(self.test_card.visible_bottom, 3)
+
+    def test_raises_valueerror_with_neutral(self):
+        # Top should go from 0 to 0
+        # Bottom should go from 1 to 2
+        self.assertRaises(ValueError, self.test_card.turn_card, 0)
