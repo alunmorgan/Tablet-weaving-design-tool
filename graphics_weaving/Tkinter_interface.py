4f291b6974be5ada04f875e9748ac4604d86bab4
alunmorgan <alun@curiousidea.co.uk>
2018-06-06 12:52:22 +0100

    Changed the output patterns to be rectangles drawn on a canvas, rather than buttons as there is no user interaction here.

--- graphics_weaving/Tkinter_interface.py
+++ graphics_weaving/Tkinter_interface.py
@@ -1,80 +1,116 @@
-from tkinter import *
-from tkinter import filedialog
+from tkinter import Tk, Canvas, Label, Button, N, S, E, W, Grid, filedialog
 from os import path
 
 
-def save_clicked():
-    print('Save clicked')
+class Interface:
+    def __init__(self):
+        self.rows = 100
+        self.columns = 100
+
+        self.PATTERN_DIR = path.join('..', 'Example_input_files')
+        self.header_rows = 3
+        self.draft_height = 4
+        self.num_cards = 8
+        self.length_turning_instructions = 16
 
 
-def load_clicked():
-    print('Load clicked')
-    file = filedialog.askopenfilename(initialdir=PATTERN_DIR, filetypes=(("Pattern files", "*.json"), ("all files", "*.*")))
+        self.window = Tk()
+        self.window.title("Tablet weaving design tool")
+
+        # self.canvas.create_rectangle(20, 20, 40, 60, fill="blue")
+
+        Label(text="Hello").grid(column=0, row=0)
+        load_btn = Button(self.window, text="Load pattern", command=self.load_clicked)
+        load_btn.grid(column=2 * self.length_turning_instructions + 1, row=0)
+        save_btn = Button(self.window, text="Save pattern", command=self.save_clicked)
+        save_btn.grid(column=2 * self.length_turning_instructions + 1, row=1)
+        set_dir_btn = Button(self.window, text="Set patterns directory", command=self.set_pattern_files_location)
+        set_dir_btn.grid(column=2 * self.length_turning_instructions + 1, row=2)
+
+        draft_grid_osft = self.header_rows + 1
+        turning_grid_ofst = self.header_rows + 1 + self.draft_height + 1
+        output_grid_top_ofst = self.header_rows + 1 + self.draft_height + 1 + self.num_cards + 1
+        output_grid_bottom_ofst = self.header_rows + 1 + self.draft_height + 1 + self.num_cards + 3
+
+        self.make_draft_grid(x_offset=1 + int(self.length_turning_instructions - self.num_cards / 2),
+                             y_offset=draft_grid_osft)
+        self.make_turning_grid(x_offset=1 + int(self.length_turning_instructions / 2),
+                               y_offset=turning_grid_ofst)
+        self.make_output_grid(x_offset=1, y_offset=output_grid_top_ofst, tb_select='Top', data=0)
+        self.make_output_grid(x_offset=1, y_offset=output_grid_bottom_ofst, tb_select='Bottom', data=0)
+
+        self.window.mainloop()
+
+    def save_clicked(self):
+        print('Save clicked')
+
+    def load_clicked(self):
+        print('Load clicked')
+        file = filedialog.askopenfilename(initialdir=self.PATTERN_DIR, filetypes=(("Pattern files", "*.json"), ("all files", "*.*")))
+
+    def set_pattern_files_location(self):
+        return filedialog.askdirectory()
+
+    def switch_turning_direction(self, arg, squares):
+        if squares[arg[0]][arg[1]].cget('bg') == 'black':
+            squares[arg[0]][arg[1]].config(bg='white')
+        elif squares[arg[0]][arg[1]].cget('bg') == 'white':
+            squares[arg[0]][arg[1]].config(bg='black')
+        else:
+            raise ValueError('The button colour should be white or black')
+
+    def make_draft_grid(self, x_offset, y_offset):
+        squares = []
+        Label(self.window, text="Pattern draft").grid(row=y_offset, columnspan=2 * self.length_turning_instructions)
+        for x in range(self.num_cards):
+            squares.append([0] * self.draft_height)
+            for y in range(self.draft_height):
+                squares[x][y] = Button(self.window, bg='black', command=lambda arg=(x, y): self.switch_turning_direction(arg, squares))
+                squares[x][y].grid(column=x + x_offset, row=(y + y_offset + 1), sticky=(N + S + E + W))
+        for x in range(self.num_cards):
+            Grid.columnconfigure(self.window, x + x_offset, weight=1)
+        for y in range(self.draft_height):
+            Grid.rowconfigure(self.window, (y + y_offset + 1), weight=1)
+
+    def make_turning_grid(self, x_offset, y_offset):
+        squares = []
+        Label(self.window, text="Turning instructions").grid(row=y_offset,
+                                                             columnspan=2 * self.length_turning_instructions)
+        for x in range(self.length_turning_instructions):
+            squares.append([0] * self.num_cards)
+            for y in range(self.num_cards):
+                squares[x][y] = Button(self.window, bg='black', command=lambda arg=(x, y): self.switch_turning_direction(arg, squares))
+                squares[x][y].grid(column=x + x_offset, row=(y + y_offset + 1), sticky=(N + S + E + W))
+        for x in range(self.length_turning_instructions):
+            Grid.columnconfigure(self.window, x + x_offset, weight=1)
+        for y in range(self.num_cards):
+            Grid.rowconfigure(self.window, (y + y_offset + 1), weight=1)
+
+    def make_output_grid(self, x_offset, y_offset, tb_select, data):
+        rect = {}
+        square_size = 25  # size in pixels
+        cellwidth = square_size
+        cellheight = square_size
+        Label(self.window, text="Output pattern (%s)" % tb_select).grid(row=y_offset,
+                                                                        columnspan=2 * self.length_turning_instructions)
+
+        canvas = Canvas(self.window, width=2 * self.length_turning_instructions * square_size,
+                        height=self.num_cards * square_size, borderwidth=0, highlightthickness=0)
+        canvas.grid(column=x_offset, row=y_offset + 1, columnspan=2 * self.length_turning_instructions)
+        for x in range(2 * self.length_turning_instructions):
+            for y in range(self.num_cards):
+                x1 = x * cellwidth
+                y1 = y * cellheight
+                x2 = x1 + cellwidth
+                y2 = y1 + cellheight
+                # TODO, fill=data[x, y])
+                rect[x, y] = canvas.create_rectangle(x1, y1, x2, y2, fill="blue", tags="rect")
+        for x in range(2 * self.length_turning_instructions):
+            Grid.columnconfigure(self.window, x + x_offset, weight=1)
+        for y in range(self.num_cards):
+            Grid.rowconfigure(self.window, (y + y_offset + 1), weight=1)
 
 
-def set_pattern_files_location():
-    return filedialog.askdirectory()
+interface1 = Interface()
 
 
-def switch_turning_direction(btn):
-    btn.bg
-
-
-def click_toggle(arg, squares):
-    if squares[arg[0]][arg[1]].cget('bg') == 'black':
-        squares[arg[0]][arg[1]].config(bg='white')
-    elif squares[arg[0]][arg[1]].cget('bg') == 'white':
-        squares[arg[0]][arg[1]].config(bg='black')
-    else:
-        raise ValueError('The button colour should be white or black')
-
-
-def make_button_grid(win, width, height, x_offset, y_offset):
-    squares = []
-    for x in range(width):
-        squares.append([0] * height)
-        for y in range(height):
-            squares[x][y] = Button(win, bg='black', command=lambda arg=(x, y): click_toggle(arg, squares))
-            squares[x][y].grid(column=x + x_offset, row=(y + y_offset + 1), sticky=(N + S + E + W))
-    for x in range(width):
-        Grid.columnconfigure(win, x + x_offset, weight=1)
-    for y in range(height):
-        Grid.rowconfigure(win, (y + y_offset + 1), weight=1)
-
-
-PATTERN_DIR = path.join('..', 'Example_input_files')
-header_rows = 3
-draft_height = 4
-num_cards = 8
-length_turning_instructions = 16
-
-window = Tk()
-window.geometry('%dx%d' % ((2 * length_turning_instructions) * 30 + 20,
-                           (header_rows + 1 + draft_height + 2 * (1 + num_cards)) * 30))
-window.title("Tablet weaving design tool")
-lbl = Label(window, text="Hello")
-lbl.grid(column=0, row=0)
-load_btn = Button(window, text="Load pattern", command=load_clicked)
-load_btn.grid(column=2 * length_turning_instructions + 1, row=0)
-save_btn = Button(window, text="Save pattern", command=save_clicked)
-save_btn.grid(column=2 * length_turning_instructions + 1, row=1)
-set_dir_btn = Button(window, text="Set patterns directory", command=set_pattern_files_location)
-set_dir_btn.grid(column=2 * length_turning_instructions + 1, row=2)
-
-
-make_button_grid(win=window, width=num_cards, height=draft_height,
-                 x_offset=1 + int(length_turning_instructions - num_cards / 2), y_offset=header_rows + 1)
-Label(window, text="").grid(row=header_rows + 1 + draft_height + 1)
-make_button_grid(win=window, width=length_turning_instructions, height=num_cards,
-                 x_offset=1 + int(length_turning_instructions / 2),
-                 y_offset=header_rows + 1 + draft_height + 1)
-Label(window, text="").grid(row=header_rows + 1 + draft_height + 1 + num_cards + 1)
-make_button_grid(win=window, width=2 * length_turning_instructions, height=num_cards, x_offset=1,
-                 y_offset=header_rows + 1 + draft_height + 1 + num_cards + 1)
-Label(window, text="").grid(row=header_rows + 1 + draft_height + 2 * (1 + num_cards) + 1)
-make_button_grid(win=window, width=2 * length_turning_instructions, height=num_cards, x_offset=1,
-                 y_offset=header_rows + 1 + draft_height + 1 + 3 * (1 + num_cards) + 1)
-Label(window, text="").grid(row=header_rows + 1 + draft_height + 1 + num_cards)
-
-
-window.mainloop()
